5edf18944cccce1ca1ce279d898bba4730d9bbd9
vduseev <vagiz.d@gmail.com>
2017-10-10 06:37:51 +0200

    Closes #2

--- src/com/duseev/intellij/preservelayout/LayoutImporter.java
+++ src/com/duseev/intellij/preservelayout/LayoutImporter.java
@@ -2,15 +2,11 @@ package com.duseev.intellij.preservelayout;
 
 import com.intellij.openapi.actionSystem.AnAction;
 import com.intellij.openapi.actionSystem.AnActionEvent;
-import com.intellij.openapi.project.*;
-import com.intellij.openapi.vfs.VirtualFile;
 
-public class LayoutExporter extends AnAction {
+public class LayoutImporter extends AnAction {
 
     @Override
     public void actionPerformed(AnActionEvent e) {
         // TODO: insert action logic here
-        Project project = e.getProject();
-        VirtualFile workspace = project.getWorkspaceFile();
     }
 }
